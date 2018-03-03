873f90826ddc354df5c9566f2cf0ad1cfb6f7d3d
sparktweak <wUdRxcDLycvsNLs148>
2018-03-03 14:10:30 +0100

    No manteinance

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('manteinance.hbs');
-});
+// app.use((req, res, next) => {
+//   res.render('manteinance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
